0490a334a0813d7425cae7d7b97eb3df94897cd3
M-Sirelbu <maivosirelbu@protonmail.com>
2025-01-20 15:31:33 +0200

    Vana kood eemaldatud, uue lehe baasfunktsionaalsus valmis.

--- code/js/asi.js
+++ code/js/asi.js
@@ -1,45 +1,16 @@
-/* MAIN */
-$(document).ready(function() {
-    /* Intro text animation */
-    let introText = "ASI KARIKAS ON VÕISTKONDLIK PROGRAMMEERIMISVÕISTLUS KOOLIÕPILASTELE.";
-    let introTextbox = $("#intro");
-
-    fitTextboxToText(introTextbox, introText, true, $("section:not(#introduction), #organizers"));
-    revealText(introTextbox, introText, 36);
-
-    /* Smooth navigation scrolling */
-    $(".nav-link").click(navScroll);
-});
-
-/* FUNCTIONS */
-/* Intro text */
-function revealText(textbox, text, speed, letter_position = 0){
-    setTimeout(function(){
-        if (letter_position < text.length){
-            textbox.append(text.charAt(letter_position));
-
-            /* Recursion until the whole text has been reveled */
-            letter_position++;
-            revealText(textbox, text, speed, letter_position);
-        }
-    }, speed);
-}
-function fitTextboxToText(textbox, text, hidden_content = false, content){
-    textbox.text(text); // Add placeholder text to measure how tall the textbox has to be
-    textbox.height(textbox.height()); // Save textbox current height
-    textbox.text(""); // Remove placeholder text
-
-    if (hidden_content){ // Content is previously hidden using CSS to avoid screen flickering
-        content.css("visibility", "visible");
+document.addEventListener("DOMContentLoaded", function() {
+    if (document.cookie == "") {
+        document.cookie = "b1";
     }
-}
-
-/* Navbar */
-function navScroll(){
-    let destination_element = $($(this).data("destination")); // Navbar link points to a ceratin page element, which ID is stored in a data label
-
-    window.scrollTo({
-        top: destination_element.offset().top - 20,
-        behavior: "smooth"
-      });
+    document.getElementById(document.cookie).className = "active";
+    document.getElementById(document.cookie+"-view").hidden = false;
+});
+function buttonClick(id) {
+    if (document.cookie != id) {
+        document.getElementById(document.cookie).className = "inactive";
+        document.getElementById(document.cookie+"-view").hidden = true;
+        document.getElementById(id).className = "active";
+        document.getElementById(id+"-view").hidden = false;
+        document.cookie = id;
+    }
 }
