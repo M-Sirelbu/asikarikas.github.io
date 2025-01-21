eb0ddd5678f52871ee9f4b99b6215b5b58dd6905
M-Sirelbu <maivosirelbu@protonmail.com>
2025-01-22 01:48:39 +0200

    added mobile support for menu

--- code/js/asi.js
+++ code/js/asi.js
@@ -1,16 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (document.cookie == "") {
-        document.cookie = "b1";
-    }
-    document.getElementById(document.cookie).className = "active";
-    document.getElementById(document.cookie+"-view").hidden = false;
-});
+var active_id = "b1"
+
 function buttonClick(id) {
-    if (document.cookie != id) {
-        document.getElementById(document.cookie).className = "inactive";
-        document.getElementById(document.cookie+"-view").hidden = true;
+    if (active_id != id) {
+        document.getElementById(active_id).className = "inactive";
         document.getElementById(id).className = "active";
+
+        document.getElementById(active_id).firstChild.className = "inactive-icon";
+        document.getElementById(id).firstChild.className = "active-icon";
+
+        document.getElementById(active_id+"-view").hidden = true;
         document.getElementById(id+"-view").hidden = false;
-        document.cookie = id;
+        active_id = id;
+    }
+}
+
+function mobileMenuButtonClick(id) {
+    for (const element of document.getElementById(id).children) {
+        element.hidden = !(element.hidden);
+    }
+    if (document.getElementById(id).className == "mobile-menu-button-inactive") {
+        document.getElementById(id).className = "mobile-menu-button-active";
+        document.getElementById("header-view").className = "header-parent";
+        document.getElementById("main-view").className = "main-parent-inactive";
+    }
+    else {
+        document.getElementById(id).className = "mobile-menu-button-inactive";
+        document.getElementById("header-view").className = "header-parent-inactive";
+        document.getElementById("main-view").className = "main-parent";
     }
 }
